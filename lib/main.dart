fcee52abcb582026306c9faec7ca09f8c2fbe208
Jheymerson Aranha <jheymersonla@gmail.com>
2025-06-02 21:07:36 -0300

    TESTEEEE

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello, iOS!', style: TextStyle(fontSize: 24)),
+    return MaterialApp(
+      title: 'Hello AltStore App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Hello AltStore'),
+      ),
+      body: const Center(
+        child: Text(
+          'Olá, Mundo AltStore!',
+          style: TextStyle(fontSize: 24),
         ),
       ),
     );
